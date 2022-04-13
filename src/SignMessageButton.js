6ecd9681e6de2ed55d9573fc033e10bcea6e8d7d
badgeth-dao <81835284+badgeth-dao@users.noreply.github.com>
2022-04-13 09:43:21 -0700

    Signing message

--- src/SignMessageButton.js
+++ src/SignMessageButton.js
@@ -1,16 +1,33 @@
-import { verifyMessage } from '@ethersproject/wallet'
 import { useConnectWallet } from '@web3-onboard/react'
+import { ethers } from 'ethers'
 import React from 'react'
 
-export const signMessage = async (provider, address, signMsg) => {
-  console.log('signMessage', provider, address, signMsg)
-  const signature = await provider.request({
-    method: 'eth_sign',
-    params: [address, signMsg]
-  })
+export const signMessage = async (provider, address) => {
+  const signMsg = ethers.utils.toUtf8Bytes('some txt message')
 
-  const recoveredAddress = verifyMessage(signMsg, signature)
-  console.log({ signMsg, signature, recoveredAddress })
+  console.log('signMessage', provider, address, signMsg)
+
+  // const signer = await provider.getSigner()
+  // console.log('signer', signer)
+  // const addr = await signer.getAddress()
+  // console.log('addr', addr)
+  // console.log('signer', signer)
+  const sig = await provider.send('personal_sign', [
+    ethers.utils.hexlify(signMsg),
+    address.toLowerCase()
+  ])
+
+  console.log({ sig })
+
+  // const signature = await provider.request({
+  //   method: 'eth_sign',
+  //   params: [address, signMsg]
+  // })
+
+  // console.log('signature', signature)
+
+  // const recoveredAddress = utils.verifyMessage(signMsg, signature)
+  // console.log({ signMsg, signature, recoveredAddress })
 }
 
 export const SignMessageButton = () => {
@@ -18,11 +35,13 @@ export const SignMessageButton = () => {
 
   const discordID = 'Some Discord ID'
 
+  const address = wallet?.accounts[0]?.address
+
   return (
     <div>
       <button
         className="bn-demo-button"
-        onClick={() => signMessage(wallet.provider, wallet.address, discordID)}
+        onClick={() => signMessage(wallet.provider, address, discordID)}
       >
         Sign Message
       </button>
